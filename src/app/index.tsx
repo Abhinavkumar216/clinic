1db0a3eae4e749337aa83eb137c79197c1f2e0ae
Abhinav <Krabhinav216@gmail.com>
2024-06-19 16:48:01 +0530

    initial commit

---
+++ src/app/index.tsx
@@ -0,0 +1,167 @@
+import { OnboardingItem, Onboardingdata } from "@/assets/Data/Onboardingdata";
+import { Colors } from "@/src/constants/Colors";
+import { Link, router } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+import React, { useRef, useState } from "react";
+import {
+  Dimensions,
+  FlatList,
+  Image,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+  ViewToken,
+} from "react-native";
+import Animated, {
+  Extrapolation,
+  FadeInDown,
+  interpolate,
+  useAnimatedStyle,
+  useSharedValue,
+  withTiming
+} from "react-native-reanimated";
+
+const { width } = Dimensions.get("window");
+const IMG_HEIGHT = 500;
+
+interface ViewableItemsChangedProps {
+  viewableItems: ViewToken[];
+  changed: ViewToken[];
+}
+
+const OnboardingScreen = () => {
+  const [currentIndex, setCurrentIndex] = useState<number>(0);
+  const flatlistRef = useRef<FlatList<number>>(null);
+
+  const progress = useSharedValue(0);
+
+  const onViewRef = useRef(({ changed }: ViewableItemsChangedProps) => {
+    if (changed[0].isViewable) {
+      setCurrentIndex(changed[0].index ?? 0);
+      progress.value = withTiming(changed[0].index ?? 0, { duration: 300 });
+    }
+  });
+
+  const scrollToIndex = (index: number) => {
+    flatlistRef.current?.scrollToIndex({ animated: true, index });
+  };
+
+  return (
+    <View>
+      <StatusBar translucent backgroundColor="transparent" />
+      <FlatList
+        ref={flatlistRef}
+        data={Onboardingdata}
+        pagingEnabled
+        viewabilityConfig={{ viewAreaCoveragePercentThreshold: 95 }}
+        onViewableItemsChanged={onViewRef.current}
+        showsHorizontalScrollIndicator={false}
+        horizontal
+        renderItem={({ item, index }) => (
+          <Card data={item} index={index} scrollToIndex={scrollToIndex} />
+        )}
+        keyExtractor={(item) => item.id}
+      />
+      <View style={styles.indicator}>
+        {Onboardingdata.map((_, index) => {
+          const animatedStyle = useAnimatedStyle(() => {
+            return {
+              width: interpolate(
+                progress.value,
+                [index - 1, index, index + 1],
+                [8, 32, 8],
+                Extrapolation.EXTEND
+              ),
+              backgroundColor: index === currentIndex ? "#1C2A3A" : "lightgray",
+            };
+          });
+          return (
+            <TouchableOpacity key={index} onPress={() => scrollToIndex(index)}>
+              <Animated.View style={[styles.indicatorCircle, animatedStyle]} />
+            </TouchableOpacity>
+          );
+        })}
+      </View>
+      <View style={{ marginTop: 25 }}>
+        <Link href={"/signin"} replace asChild>
+          <Text style={styles.subHeading}>Skip</Text>
+        </Link>
+      </View>
+    </View>
+  );
+};
+
+type DataProps = {
+  data: OnboardingItem;
+  index: number;
+  scrollToIndex: (index: number) => void;
+};
+
+const Card = ({ data, index, scrollToIndex }: DataProps) => {
+  const onPress = () => {
+    if (index == 2) {
+      router.replace("/signin");
+    } else {
+      scrollToIndex(index + 1);
+    }
+  };
+  return (
+    <View>
+      <Image source={data.Image} style={styles.image} />
+      <View style={styles.bottomContainer}>
+        <Animated.Text entering={FadeInDown} style={styles.heading}>
+          {data.title}
+        </Animated.Text>
+        <Animated.Text entering={FadeInDown} style={styles.subHeading}>
+          {data.subtitle}
+        </Animated.Text>
+        <TouchableOpacity style={styles.button} onPress={onPress}>
+          <Text style={styles.buttonText}>{index == 2 ? "Done" : "Next"}</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+};
+
+export default OnboardingScreen;
+
+const styles = StyleSheet.create({
+  image: { width, height: IMG_HEIGHT },
+  bottomContainer: {
+    alignItems: "center",
+    paddingHorizontal: 16,
+    marginTop: 10,
+    width,
+  },
+  heading: { fontSize: 24, 
+    // fontFamily: "InterBold", 
+    marginVertical: 15 },
+  subHeading: {
+    textAlign: "center",
+    // fontFamily: "InterRegular",
+    fontSize: 16,
+    lineHeight: 26,
+    color: "374151",
+  },
+  button: {
+    backgroundColor: "#1C2A3A",
+    width: (width * 80) / 100,
+    alignItems: "center",
+    paddingVertical: 15,
+    borderRadius: 25,
+    marginTop: 25,
+  },
+  buttonText: {
+    // fontFamily: "InterMedium",
+    fontSize: 16,
+    color: Colors.light.white
+  },
+  indicator: {
+    flexDirection: "row",
+    justifyContent: "center",
+    marginTop: 35,
+    // borderWidth: 1,
+  },
+  indicatorCircle: { height: 8, borderRadius: 100, marginRight: 5 },
+});
---
+++ src/constants/Colors.ts
@@ -0,0 +1,42 @@
+/**
+ * Below are the colors that are used in the app. The colors are defined in the light and dark mode.
+ * There are many other ways to style your app. For example, [Nativewind](https://www.nativewind.dev/), [Tamagui](https://tamagui.dev/), [unistyles](https://reactnativeunistyles.vercel.app), etc.
+ */
+
+
+export const Colors = {
+  light: {
+    Primary: "#1C2A3A",
+    white: "#FFFFFF",
+    Grey50: "#F9FAFB",
+    Grey100: "#F3F4F6",
+    Grey200: "#E5E7EB",
+    Grey300: "#D1D5DB",
+    Grey400: "#9CA3AF",
+    Grey500: "#6B7280",
+    Grey600: "#4B5563",
+    Grey700: "#374151",
+    Grey800: "#1F2A37",
+    Grey900: "#111928",
+    DeepTeal: "#014737",
+    Teal: "#4D9B91",
+    LightTeal: "#A4CFC3",
+    Green: "#93C19E",
+    PaleGreen: "#DEF7E4",
+    DarkRed: "#771D1D",
+    DeepPink: "#DC9497",
+    Pink: "#DEB6B5",
+    LightPink: "#FDE8E8",
+    LightPurple: "#ACA1CD",
+    Blue: "#1C64F2",
+    PaleBlue: "#89CCDB",
+    Purple: "#352261",
+    Orange: "#F5AD7E",
+    success: "#439F48",
+    info: "#B8B8BB",
+    warning: "#FB8A00",
+    error: "#F54337",
+  },
+  dark: {
+  },
+};
